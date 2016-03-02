6b53ed9764f615ba0ea19b9e04e35deb429ed8ac
Jacqueline-G <garruto.j@husky.neu.edu>
2016-03-01 19:43:46 -0500

    initial commit

---
+++ Class 7/sketch.js
@@ -0,0 +1,19 @@
+var listOfNames0 = ["Talia", "Joana", "Amy"];
+var listOfNames1 = ["bill", "Bill", "Bill"];
+var listOfNames2 = ["Joe", "bill", "George"];
+var listOfnames3 = ["Amy", "Jane", "Jill"];
+var listOfnames4 = ["Bill"];
+
+var checkNames = function (anArray){
+for (var i = 0 , i < anArray.length, i ++)
+if (anArray.length === 1){
+return false;
+}
+
+if (i > 0){
+if(anArray[i] === anArray[i+1]{
+return true;
+    }
+  }
+}
+console.log (isSametwice(listOfnames0));
