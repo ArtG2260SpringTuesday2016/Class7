9e80a0d7191149860bb5ec42dbc3e9cf707c7693
heyryanleys <ryanleys@gmail.com>
2016-03-01 20:03:35 -0500

    Fixed Code

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
-var names = ["Bowen","Ryan","Ryan","Annie","Leys"]
+var listOfNames0 = ["Talia", "Joana", "Amy"];
+var listOfNames1 = ["bill", "Bill", "Bill"];
+var listOfNames2 = ["Joe", "bill", "George"];
+var listOfNames3 = ["Amy", "Jane", "Jill"];
+var listOfNames4 = ["Bill"];
+var listOfNames5 = ["Bill", "bill", "Bill", undefined]
 
 var checkNames = function(anArray){
   for(var i = 0; i < anArray.length; i++){
     if(anArray.length === 1){
       return false;
     }
-  if (i > 0){
-      if(anArray[i] === anArray[i+1])
+  else if (i > 0){
+      if(anArray[i] === anArray[i + 1])
       return true;
-    }
+  else
+    return false;
+  }
   }
 }
 
-checkNames(names)
-console.log(checkNames(names))
+//checkNames(listOfNames0) -> false
+//checkNames(listOfNames1) -> true
+//checkNames(listOfNames2) -> false
+//checkNames(listOfNames3) -> false
+//checkNames(listOfNames4) -> false
+//checkNames(listOfnames5)  -> false
+
+console.log(checkNames(listOfNames0))
