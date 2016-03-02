16fdc1a762e341a7a5828bf830b3b49dd849dd6a
MistaFish31 <zhang.ma@husky.neu.edu>
2016-03-01 19:36:48 -0500

    update variable names

--- sketch.p5/sketch.js
+++ sketch.p5/sketch.js
@@ -1,23 +1,25 @@
-var fruits0=["blueberry", "apple", "peach"];
-var fruits1=["blueberry", "apple", "apple"];
-var fruits2=["peach","blueberry", "strawberry"];
-var fruits3=["kiwi", "potato", "tomato"];
+var listofNames0=["blueberry", "apple", "peach"];
+var listofNames1=["blueberry", "apple", "apple"];
+var listofNames2=["peach","blueberry", "strawberry"];
+var listofNames3=["kiwi", "potato", "tomato"];
 
-var checkFruits=function(anArray){
+var isSameNameTwice=function(anArray){
   for (var i=0; i<anArray.length; i++){
     if(anArray.length===1){
       return false;
     }
-    if(i<0){
+    if(i>0){
       if(anArray[i]===anArray[i+1]){
         return true;
+      }
+      else{
+        return false;
       }
     }
   }
 }
 
-
-function draw() {
-  console.log(checkFruits(fruits0))
-  
-}
+console.log(isSameNameTwice(listofNames0))
+console.log(isSameNameTwice(listofNames1))
+console.log(isSameNameTwice(listofNames2))
+console.log(isSameNameTwice(listofNames3))
