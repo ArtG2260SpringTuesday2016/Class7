1313cae942a5339385da213cbd2fa474b55e6084
heyryanleys <ryanleys@gmail.com>
2016-03-01 20:04:49 -0500

    Simplified Code

--- sketch.js
+++ sketch.js
@@ -7,10 +7,7 @@ var listOfNames5 = ["Bill", "bill", "Bill", undefined]
 
 var checkNames = function(anArray){
   for(var i = 0; i < anArray.length; i++){
-    if(anArray.length === 1){
-      return false;
-    }
-  else if (i > 0){
+    if (i > 0){
       if(anArray[i] === anArray[i + 1])
       return true;
   else
@@ -26,4 +23,4 @@ var checkNames = function(anArray){
 //checkNames(listOfNames4) -> false
 //checkNames(listOfnames5)  -> false
 
-console.log(checkNames(listOfNames0))
+console.log(checkNames(listOfNames5))
