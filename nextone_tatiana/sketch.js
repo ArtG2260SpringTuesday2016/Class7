ce37f50637bdb4b3c8a8baa47a974939fd0191c1
tatianafontalvo <fontalvo.t@husky.neu.edi>
2016-03-01 19:50:16 -0500

    yayitworks
    woo

---
+++ nextone_tatiana/sketch.js
@@ -0,0 +1,31 @@
+var listOfNames0 = ["Talia", "Joana", "Amy"];
+var listOfNames1 = ["bill", "Bill", "Bill"];
+var listOfNames2 = ["Joe", "Bill", "George"];
+var listOfNames3 = ["Amy", "Joana", "Jill"];
+
+
+  var isSameNameTwice  = function(anArray){
+  for(var i = 0; i < anArray.length; i++){
+    if(anArray.length === 1){
+      return false;
+    }
+  if(i>0) {
+    if(anArray[i] === anArray[i+1]){
+      return true;
+    }
+    else {
+      return false;}
+    }
+  }
+  isSameNameTwice(listOfNames);
+}
+
+
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
