fdb47f784ca29a12eb2ec622e2ecb29cf1b5ea8f
Zhantao Peng <peng.z@husky.neu.edu>
2016-03-01 20:18:11 -0500

    name

---
+++ listofnames/sketch.js
@@ -0,0 +1,12 @@
+var listOfNames0 = ["Talia", "Joana", "Amy"];
+var listOfNames1 = ["Bill", "Bill", "Bill"];
+var listOfNames2 = ["Joe", "bill", "George"];
+var listOfnames3 = ["Amy", "Jane", "Jill"];
+
+var checkname = function(listOfNames0,listOfName1){
+for(var i = 0; i < listOfNames1;i++){
+  if (listOfNames1[i] == listOfNames1[0]){
+  return true
+}return false
+}
+}
