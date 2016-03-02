450bfdf6249a253e577f739913e29429da28fb26
AlexisDiefenbach <aodiefenbach@gmail.com>
2016-03-01 19:56:19 -0500

    class7 1

---
+++ sketch.js/sketch.js
@@ -0,0 +1,28 @@
+var listOfNames0 = ["Talia", "Joana", "Amy"];
+var listOfNames1 = ["bill", "Bill", "Bill"];
+var listOfNames2 = ["Joe", "bill", "George"];
+var listOfnames3 = ["Amy", "Jane", "Jill"];
+
+var isSameNameTwice = function (some) {
+  for(var i=0;i<some.length;i++){
+    if (some.length [i] ===1){
+      return false;
+    }
+    else if (i>0){
+      if (some[i] === some[i+1]){
+        return true;}
+        else
+        return false;
+      }
+    }
+  }
+
+function setup() {
+  
+
+  
+}
+
+function draw() {
+  
+}
