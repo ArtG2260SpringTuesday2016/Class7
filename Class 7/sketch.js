0c8890e6bb95230b0b17566198da9d58da84821e
Jacqueline-G <garruto.j@husky.neu.edu>
2016-03-01 20:02:34 -0500

    sort of working

--- Class 7/sketch.js
+++ Class 7/sketch.js
@@ -4,16 +4,20 @@ var listOfNames2 = ["Joe", "bill", "George"];
 var listOfnames3 = ["Amy", "Jane", "Jill"];
 var listOfnames4 = ["Bill"];
 
-var checkNames = function (anArray){
-for (var i = 0 , i < anArray.length, i ++)
-if (anArray.length === 1){
-return false;
+var isSamenametwice = function(anArray){
+  for(var i = 0; i < anArray.length; i ++){
+    if(anArray.length === 1){
+      return false;
+}
 }
 
-if (i > 0){
-if(anArray[i] === anArray[i+1]{
-return true;
+if(i > 0){
+  if(anArray[i] === anArray[i+1]){
+    return true;
     }
+    else{
+    return false;}
   }
 }
-console.log (isSametwice(listOfnames0));
+console.log(isSamenametwice(listOfNames1));
+
