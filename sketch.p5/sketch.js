f1cc9028a244ed11e7ee2c05d8e05b1831147613
MistaFish31 <zhang.ma@husky.neu.edu>
2016-03-01 20:07:00 -0500

    update to ignore undefined

--- sketch.p5/sketch.js
+++ sketch.p5/sketch.js
@@ -3,6 +3,8 @@ var listofNames1=["bill", "Bill", "Bill"];
 var listofNames2=["Joe", "Bill", "George"];
 var listofNames3=["Amy", "Jane", "Jill"];
 var listofNames4=["Bill"];
+var listofNames5=["Bill","bill", "Bill", undefined];
+var listofNames6=["Bill","bill", "Bill", undefined, undefined];
 
 var isSameNameTwice=function(anArray){
   for (var i=0; i<anArray.length; i++){
@@ -10,7 +12,7 @@ var isSameNameTwice=function(anArray){
       return false;
     }
     if(i>0){
-      if(anArray[i]===anArray[i+1]){
+      if(anArray[i]===anArray[i+1] && i!==undefined){
         return true;
       }
       else{
@@ -25,3 +27,5 @@ console.log(isSameNameTwice(listofNames1))
 console.log(isSameNameTwice(listofNames2))
 console.log(isSameNameTwice(listofNames3))
 console.log(isSameNameTwice(listofNames4))
+console.log(isSameNameTwice(listofNames5))
+console.log(isSameNameTwice(listofNames6))
