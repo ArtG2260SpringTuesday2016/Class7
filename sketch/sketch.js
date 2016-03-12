e6242f6a9a206d0015ba7774d45a966f3172e8d5
FlyingPig13 <anotherhale.m@gmail.com>
2016-03-12 02:18:36 -0500

    Quick Fix
    Realized old code wouldn't work if the array started with two matching
    non-strings.

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -2,10 +2,10 @@ var isSame = function(ar) {
   var hold = ar.length;
   for (i = 0; i <= ar.length; i++) {
     hold--;
-    if (ar.length <= 1 || typeof ar[i] != "string"){
+    if (ar.length <= 1){
     return false;
     }
-    else if (ar[i] === ar[i + 1]){
+    else if (ar[i] === ar[i + 1]  && typeof ar[i] == "string"){
       return true;
     }
     else if (hold === 0) {
@@ -22,3 +22,4 @@ console.log(isSame([]));
 console.log(isSame([2, 3, 4, 5, 1, 1]));
 console.log(isSame([undefined]));
 console.log(isSame(["Bill", "bill", "Bill", undefined, undefined]));
+console.log(isSame([undefined, undefined,"Bill", "Bill", "Bill"]));
